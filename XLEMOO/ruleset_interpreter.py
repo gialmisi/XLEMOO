13ef4c779dd88e4ac3ab5a79c6d66f57824e6396
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2022-08-31 20:45:22 +0300

    Continued working on rule set instantiator. Implemented rule extractor for SlipperClassifier.

--- XLEMOO/ruleset_interpreter.py
+++ XLEMOO/ruleset_interpreter.py
@@ -1,6 +1,34 @@
 from typing import List, Dict, Tuple
+from imodels import SlipperClassifier
 import numpy as np
 
 
-def instantiate_ruleset_rules(rules: Dict[Tuple[str, str], str]) -> np.ndarray:
-    return np.zeros(1)
+def extract_slipper_rules(
+    classifier: SlipperClassifier,
+) -> Tuple[List[Dict[Tuple[str, str], str]], List[float]]:
+    """Given a trained SlipperClassifier, extracts the trained rules alongside the weight for each rule.
+    The rules are returned in a list of dictionaries. Each rule is represented by one
+    dictionary. Each dictionary is of the format:
+
+    {("feature_name", "comparison_op"): "value"} where comparison_op can be "<", "<=", ">", or ">=".
+
+    The weigth represents the importance of each rule. The feature names are expected to be of the format
+    "x_i" where 'i' is zero-indexed (first feature is 'x_0' etc.).
+    """
+    weights = classifier.estimator_weights_
+    raw_rules = classifier.rules_
+
+    rules = [rule.agg_dict for rule in raw_rules]
+
+    return rules, weights
+
+
+def instantiate_ruleset_rules(
+    rules: List[Dict[Tuple[str, str], str]],
+    weights: List[float],
+    n_features: int,
+    feature_limits: List[Tuple[float, float]],
+    n_samples: int,
+) -> np.ndarray:
+
+    return
--- tests/test_ruleset_interpreter.py
+++ tests/test_ruleset_interpreter.py
@@ -1,12 +1,51 @@
 import numpy as np
 import numpy.testing as npt
 import pytest
-from XLEMOO.ruleset_interpreter import instantiate_ruleset_rules
+from dataclasses import dataclass
+from XLEMOO.ruleset_interpreter import instantiate_ruleset_rules, extract_slipper_rules
+from typing import List, Tuple, Dict
 
 
-@pytest.mark.ruleset
+@dataclass
+class DummyRule:
+    agg_dict: Dict[Tuple[str, str], str]
+
+
+@dataclass
+class DummySlipper:
+    estimator_weights_: List[float]
+    rules_: List[DummyRule]
+
+
+@pytest.fixture
+def dummy_slipper_classifier():
+    rule_dicts = [
+        {("x_2", "<"): "0.2", ("x_1", ">="): "0.9"},
+        {("x_2", "<"): "0.2", ("x_1", ">="): "0.9", ("x_3", "<"): "0.1"},
+        {("x_1", ">"): "0.8", ("x_3", "<="): "0.3"},
+    ]
+    weights = [0.5, 0.3, 0.9]
+
+    dummy_rule_list = [DummyRule(rule_dict) for rule_dict in rule_dicts]
+
+    dummy_classifier = DummySlipper(weights, dummy_rule_list)
+
+    return dummy_classifier
+
+
+@pytest.mark.rulesets
 def test_instantiate_ruleset_rules():
-    dummy_dict = {("X_1", "<"): "1000", ("X_2", ">="): "300"}
-    res = instantiate_ruleset_rules(dummy_dict)
+    pass
 
-    npt.assert_almost_equal(res, np.array([0]))
+
+@pytest.mark.rulesets
+def test_extract_slipper_rules(dummy_slipper_classifier):
+    rules, weights = extract_slipper_rules(dummy_slipper_classifier)
+
+    assert len(rules) == len(weights)
+
+    for (i, rule) in enumerate(rules):
+        assert rule == dummy_slipper_classifier.rules_[i].agg_dict
+
+    for (i, weight) in enumerate(weights):
+        npt.assert_almost_equal(dummy_slipper_classifier.estimator_weights_[i], weight)
