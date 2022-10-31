c8c9d31d6b17761d3dfc7b58f609e2d6c9ed661a
Giovanni Misitano <giovanni.misitano@jyu.fi>
2022-10-31 15:55:54 +0200

    Script file ready to start running experiments. Also config file is done. Need to specify Snakemake rules next and the start running.

--- scripts/run_w_freq_and_split.py
+++ scripts/run_w_freq_and_split.py
@@ -11,9 +11,10 @@ from desdeo_tools.scalarization.ASF import PointMethodASF
 from desdeo_emo.recombination import SBX_xover, BP_mutation
 from imodels import SkopeRulesClassifier
 import json
+from datetime import datetime
 
 n_objectives = 3
-n_of_variables = 5
+n_variables = 5
 
 nadir = np.array([1700.0, 12.0, 0.2])
 ideal = np.array([1600.0, 6.0, 0.038])
@@ -21,54 +22,80 @@ ideal = np.array([1600.0, 6.0, 0.038])
 ref_point = np.array([1650.0, 7.0, 0.05])
 
 problem = vehicle_crashworthiness()
+problem_name = "vehicle crash worthiness"
 
-fitness_fun = asf_wrapper(
-    PointMethodASF(nadir=nadir, ideal=ideal), {"reference_point": ref_point}
-)
+fitness_fun = asf_wrapper(PointMethodASF(nadir=nadir, ideal=ideal), {"reference_point": ref_point})
+fitness_fun_name = "PointMethodASF"
+
+use_darwin = True
+use_ml = True
+ml_probe = None
+ml_threshold = None
+darwin_probe = None
+darwin_threshold = None
+total_iterations = 5  # TODO: must be calculated
 
 lem_params = LEMParams(
-    use_darwin=True,
-    use_ml=True,
+    use_darwin=use_darwin,
+    use_ml=use_ml,
     fitness_indicator=fitness_fun,
-    ml_probe=1,
-    ml_threshold=None,
-    darwin_probe=5,
-    darwin_threshold=None,
-    total_iterations=5,  # TODO: must be calculated
+    ml_probe=ml_probe,
+    ml_threshold=ml_threshold,
+    darwin_probe=darwin_probe,
+    darwin_threshold=darwin_threshold,
+    total_iterations=total_iterations,
 )
 
 pop_size = 200
+cross_over_op = SBX_xover()
+mutation_op = BP_mutation(problem.get_variable_lower_bounds(), problem.get_variable_upper_bounds())
+selection_op = SelectNBest(None, pop_size)
+populatin_init_design = "LHSDesign"
+ea_iterations_per_cycle = 1
 
 ea_params = EAParams(
     population_size=pop_size,
-    cross_over_op=SBX_xover(),
-    mutation_op=BP_mutation(
-        problem.get_variable_lower_bounds(), problem.get_variable_upper_bounds()
-    ),
-    selection_op=SelectNBest(None, pop_size),
-    population_init_design="LHSDesign",
-    iterations_per_cycle=1,  # TODO: must be calculated,
+    cross_over_op=cross_over_op,
+    mutation_op=mutation_op,
+    selection_op=selection_op,
+    population_init_design=populatin_init_design,
+    iterations_per_cycle=ea_iterations_per_cycle,
 )
 
-# TODO: document me
+ml_model_name = "SkopedRuleClassifier"
+ml_precision_min = 0.1
+ml_n_estimators = 30
+ml_max_features = None
+ml_max_depth = None
+ml_bootstrap = True
+ml_bootstrap_features = True
+
 ml = SkopeRulesClassifier(
-    precision_min=0.1,
-    n_estimators=30,
-    max_features=None,
-    max_depth=None,
-    bootstrap=True,
-    bootstrap_features=True,
+    precision_min=ml_precision_min,
+    n_estimators=ml_n_estimators,
+    max_features=ml_max_features,
+    max_depth=ml_max_depth,
+    bootstrap=ml_bootstrap,
+    bootstrap_features=ml_bootstrap_features,
 )
+
+h_split = 0.2
+l_split = 0.2
+instantation_factor = 10
+generation_lookback = 0
+ancestral_recall = 0
+unique_only = True
+ml_iterations_per_cycle = 1
 
 ml_params = MLParams(
-    H_split=0.20,
-    L_split=0.10,
+    H_split=h_split,
+    L_split=l_split,
     ml_model=ml,
-    instantation_factor=10,  # TODO: document me
-    generation_lookback=0,
-    ancestral_recall=0,
-    unique_only=True,
-    iterations_per_cycle=1,  # TODO: must be calculated
+    instantation_factor=instantation_factor,
+    generation_lookback=generation_lookback,
+    ancestral_recall=ancestral_recall,
+    unique_only=unique_only,
+    iterations_per_cycle=ml_iterations_per_cycle,
 )
 
 lemoo = LEMOO(problem, lem_params, ea_params, ml_params)
@@ -76,6 +103,59 @@ lemoo = LEMOO(problem, lem_params, ea_params, ml_params)
 lemoo.run_iterations()
 
 data = {"individuals": [], "fitness_fun_values": [], "objective_values": []}
+
+info = {
+    "date": datetime.today().strftime("%Y-%m-%d"),
+    "problem": problem_name,
+    "n_variables": n_variables,
+    "n_objectives": n_objectives,
+    "ideal": ideal.tolist(),
+    "nadir": nadir.tolist(),
+    "ref_point": ref_point.tolist(),
+    "fitness_fun": fitness_fun_name,
+}
+
+data["info"] = info
+
+parameters = {
+    "lemoo": {
+        "use_darwin": use_darwin,
+        "use_ml": use_ml,
+        "ml_probe": ml_probe,
+        "ml_threshold": ml_threshold,
+        "darwin_probe": darwin_probe,
+        "darwin_threshold": darwin_threshold,
+        "total_iterations": total_iterations,
+    },
+    "ea_mode": {
+        "pop_size": pop_size,
+        "cross_over_op": str(cross_over_op),
+        "mutation_op": str(mutation_op),
+        "selection_op": str(selection_op),
+        "populatin_init_design": populatin_init_design,
+        "ea_iterations_per_cycle": ea_iterations_per_cycle,
+    },
+    "ml_model": {
+        "ml_model_name": ml_model_name,
+        "ml_precision_min": ml_precision_min,
+        "ml_n_estimators": ml_n_estimators,
+        "ml_max_features": ml_max_features,
+        "ml_max_depth": ml_max_depth,
+        "ml_bootstrap": ml_bootstrap,
+        "ml_bootstrap_features": ml_bootstrap_features,
+    },
+    "ml_mode": {
+        "h_split": h_split,
+        "l_split": l_split,
+        "instantation_factor": instantation_factor,
+        "generation_lookback": generation_lookback,
+        "ancestral_recall": ancestral_recall,
+        "unique_only": unique_only,
+        "ml_iterations_per_cycle": ml_iterations_per_cycle,
+    },
+}
+
+data["parameters"] = parameters
 
 for gen in lemoo._generation_history:
     data["individuals"].append(gen.individuals.tolist())
