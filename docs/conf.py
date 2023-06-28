0ecaa8cb9496f413d0251c8f25f40e307e6bd4f3
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2023-06-28 10:11:01 +0300

    Added kernel name to docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,8 @@ sys.path.insert(0, os.path.abspath('../'))
 
 extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'nbsphinx']
 
+nbsphinx_kernel_name = "python3"
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'notebooks/old_notebooks/*']
 source_suffix = {'.rst': 'restructuredtext'}
